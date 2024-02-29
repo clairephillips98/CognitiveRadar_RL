a7e6c832f37666b66f4acfe60cbe51a65d8fd772
clairephillips98 <clairephillips98@gmail.com>
2024-02-29 16:25:11 -0500

    update sh

--- gpu_serial_job.sh
+++ gpu_serial_job.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-salloc --time=0:15:00 --mem=4G --gres=gpu:1 --ntasks=1 --account=def-rsadve create_env.sh
+salloc --time=0:15:00 --mem=4G --gres=gpu:1 --ntasks=1 --account=def-rsadve ./create_env.sh
 
