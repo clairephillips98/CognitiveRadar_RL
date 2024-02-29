b6edc55d2157056868880645130855b929df53b1
clairephillips98 <clairephillips98@gmail.com>
2024-02-29 14:01:23 -0500

    submit job

--- gpu_serial_job.sh
+++ gpu_serial_job.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
-#SBATCH --account=def-someuser
-#SBATCH --gpus-per-node=1
-#SBATCH --mem=4000M               # memory per node
-#SBATCH --time=0-03:00
-chmod -x create_env.sh
-sh create_env.sh                         # you can use 'nvidia-smi' for a test
+salloc --time=0:15:00 --mem=4G --gres=gpu:1 --ntasks=1 --account=def-rsadve create_env.sh
+
