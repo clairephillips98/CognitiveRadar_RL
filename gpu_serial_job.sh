b5991a6dfa8c82de1a75f73a64089d11eb2a4be5
clairephillips98 <clairephillips98@gmail.com>
2024-02-29 13:54:12 -0500

    first try at gpu job

---
+++ gpu_serial_job.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#SBATCH --account=def-someuser
+#SBATCH --gpus-per-node=1
+#SBATCH --mem=4000M               # memory per node
+#SBATCH --time=0-03:00
+chmod -x create_env.sh
+./create_env.sh                         # you can use 'nvidia-smi' for a test
