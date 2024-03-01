14ed2f744be35d71763b7b88fc229b4479a88e5e
clairephillips98 <clairephillips98@gmail.com>
2024-03-01 16:14:57 -0500

    requesting more time and making the requirements shorter

--- gpu_serial_job.sh
+++ gpu_serial_job.sh
@@ -3,5 +3,5 @@
 echo pwd
 chmod +x create_env.sh
 
-salloc --time=0:15:00 --mem=4G --gres=gpu:1 --ntasks=1 create_env.sh cedar
+salloc --time=2:00:00 --mem=4G --gres=gpu:1 --ntasks=1 create_env.sh cedar
 
