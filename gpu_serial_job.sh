fc906ffc6024927475358d649ef650d8ffffbda8
clairephillips98 <clairephillips98@gmail.com>
2024-03-01 15:06:59 -0500

    add cedar param in gpu serial job

--- gpu_serial_job.sh
+++ gpu_serial_job.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-salloc --time=0:15:00 --mem=4G --gres=gpu:1 --ntasks=1 --account=def-rsadve ./create_env.sh
+
+echo pwd
+
+salloc --time=0:15:00 --mem=4G --gres=gpu:1 --ntasks=1 --account=def-rsadve ./create_env.sh cedar
 
