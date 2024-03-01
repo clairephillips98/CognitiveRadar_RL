36b689a6c7edbc1f5e62bb0f8ddae0accc529228
clairephillips98 <clairephillips98@gmail.com>
2024-03-01 16:25:10 -0500

    dont recreate venv each time maybe?

--- create_env.sh
+++ create_env.sh
@@ -1,22 +1,25 @@
 #!/bin/bash
 
 # Check if the environment is "cedar"
-if [ "$1" = "cedar" ]; then
+if [ "$1" = "cedar_new" ]; then
     module load StdEnv/2020 python/3.11
+
+  # Specify Python 3.11
+
+  # Create the virtual environment
+  python3 -m venv myenv
+
+  # Activate the virtual environment
+  source myenv/bin/activate
+
+  pip3 install -r requirements.txt
 fi
 
-# Specify Python 3.11
-
-# Create the virtual environment
-python3 -m venv myenv
-
-# Activate the virtual environment
-source myenv/bin/activate
-
-pip3 install -r requirements.txt
-
+if [ "$1" = "cedar_new" ]; then
+  source myenv/bin/activate
+fi
 # Verify the Python version
 
-python --version
+echo python --version
 
 python3 -m rl_agents.DRL_code_pytorch.Rainbow_DQN.Rainbow_DQN_radar_main
