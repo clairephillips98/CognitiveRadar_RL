95e3143ea27e6937963a186ee87c6f2810d01d40
clairephillips98 <clairephillips98@gmail.com>
2024-03-04 10:11:32 -0500

    change time alloc

--- gpu_serial_job.sh
+++ gpu_serial_job.sh
@@ -2,5 +2,5 @@
 
 echo pwd
 chmod +x create_env.sh
-salloc --time -m=2:00:00 --mem=4G --gpus-per-node=1 --ntasks=1 create_env.sh cedar
+salloc --time -m=02:00:00 --mem=4G --gpus-per-node=1 --ntasks=1 create_env.sh cedar
 
