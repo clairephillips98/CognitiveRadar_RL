3b74575c68b15aff4db92843cd82e815d30c84a8
clairephillips98 <clairephillips98@gmail.com>
2024-03-04 16:05:27 -0500

    niagara resource

--- gpu_serial_job.sh
+++ gpu_serial_job.sh
@@ -3,5 +3,5 @@
 echo pwd
 chmod +x create_env.sh
 
-salloc --time=2:00:00 --mem=2G --gpus-per-node=1 --ntasks=1 --mail-user=clairevphillips98@gmail.com create_env.sh cedar
+salloc --time=2:00:00 --mem=2G --gpus-per-node=1 --ntasks=1 --mail-user=clairevphillips98@gmail.com create_env.sh
 
