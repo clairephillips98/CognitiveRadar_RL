24949833b285a70604da1055e11dc96784763b2b
Claire Phillips <cphil@mist-login01.scinet.local>
2024-03-07 12:46:56 -0500

    working submit

--- gpu_serial_job.sh
+++ gpu_serial_job.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
-
-echo pwd
-chmod +x create_env.sh
-
-salloc --time=2:00:00 --mem=2G --gpus-per-node=1 --ntasks=1 --mail-user=clairevphillips98@gmail.com create_env.sh
+#SBATCH --nodes=
+#SBATCH --gpus-per-node=
+#SBATCH --time=1:00:0
+#SBATCH --account=soscip-<SOSCIP_PROJECT_ID> #For SOSCIP projects only
+module load anaconda3
+echo 1
+source activate pytorch_env
+echo 2
+echo $PYTHONPATH
+echo 2
+pip list
+echo 3
+conda list
+python -m check_packages.py 
+python3 -m rl_agents.baseline_model
+echo 11
+python3 -m rl_agents.DRL_code_pytorch.Rainbow_DQN.Rainbow_DQN_radar_main
+ 
 
