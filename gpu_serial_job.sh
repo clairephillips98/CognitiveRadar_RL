b49b0185c7b5ec690a3aa345afd093deee6c0270
clairephillips98 <clairephillips98@gmail.com>
2024-03-07 23:21:05 -0500

    big trial

--- gpu_serial_job.sh
+++ gpu_serial_job.sh
@@ -13,9 +13,19 @@ echo 2
 pip list
 echo 3
 conda list
-python -m check_packages.py 
-python3 -m rl_agents.baseline_model
-echo 11
+echo 4
 python3 -m rl_agents.DRL_code_pytorch.Rainbow_DQN.Rainbow_DQN_radar_main
- 
-
+echo everything
+python3 -m rl_agents.DRL_code_pytorch.Rainbow_DQN.Rainbow_DQN_radar_main
+echo nothing
+python3 -m rl_agents.DRL_code_pytorch.Rainbow_DQN.Rainbow_DQN_radar_main --use_double=0 --use_dueling=0 --use_noisy=0 --use_per=0 --use_n_steps=0
+echo use_double
+python3 -m rl_agents.DRL_code_pytorch.Rainbow_DQN.Rainbow_DQN_radar_main --use_double=1 --use_dueling=0 --use_noisy=0 --use_per=0 --use_n_steps=0
+echo use_dueling
+python3 -m rl_agents.DRL_code_pytorch.Rainbow_DQN.Rainbow_DQN_radar_main --use_double=0 --use_dueling=1 --use_noisy=0 --use_per=0 --use_n_steps=0
+echo use_noisy
+python3 -m rl_agents.DRL_code_pytorch.Rainbow_DQN.Rainbow_DQN_radar_main --use_double=0 --use_dueling=0 --use_noisy=1 --use_per=0 --use_n_steps=0
+echo use_per
+python3 -m rl_agents.DRL_code_pytorch.Rainbow_DQN.Rainbow_DQN_radar_main --use_double=0 --use_dueling=0 --use_noisy=0 --use_per=1 --use_n_steps=0
+echo use_n_steps
+python3 -m rl_agents.DRL_code_pytorch.Rainbow_DQN.Rainbow_DQN_radar_main --use_double=0 --use_dueling=0 --use_noisy=0 --use_per=0 --use_n_steps=1
