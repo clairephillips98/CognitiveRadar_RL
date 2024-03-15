56c92ebc83f2a072907bef25bbbcce3ceddfebb8
clairephillips98 <clairephillips98@gmail.com>
2024-03-15 13:47:13 -0400

    debug

--- rl_agents/DRL_code_pytorch/Rainbow_DQN/replay_buffer.py
+++ rl_agents/DRL_code_pytorch/Rainbow_DQN/replay_buffer.py
@@ -128,6 +128,8 @@ class Prioritized_ReplayBuffer(object):
         batch = {}
         for key in self.buffer.keys():  # numpy->tensor
             if key == 'action':
+                print(self.buffer[key])
+                print(type(self.buffer[key]))
                 batch[key] = self.buffer[key][batch_index].to(torch.long)
             else:
                 batch[key] = self.buffer[key][batch_index].to(torch.float32)
