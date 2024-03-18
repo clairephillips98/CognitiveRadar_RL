f3d90f1dddcec93dfb802acfe5522cbb3ce61533
clairephillips98 <clairephillips98@gmail.com>
2024-03-18 15:03:07 -0400

    n tasks

--- gpu_serial_job.sh
+++ gpu_serial_job.sh
@@ -5,20 +5,16 @@
 #SBATCH --export=ALL
 #SBATCH --output=cphil_test.txt
 #SBATCH --partition=compute_full_node
+#SBATCH --ntasks=4
 module load anaconda3
 echo 1
 source activate pytorch_env
 
-echo baselines
-python -m rl_agents.baseline_model --baseline_model_type='min_variance' --cdl=0.9 &
-python -m rl_agents.baseline_model --baseline_model_type='max_variance' --cdl=0.9 &
-python -m rl_agents.baseline_model --baseline_model_type='simple' --cdl=0.9
-wait
 echo next 4
-python -m rl_agents.DRL_code_pytorch.Rainbow_DQN.Rainbow_DQN_radar_main --blur_radius=1 --cdl=0.9 --epsilon_init=1 &
-python -m rl_agents.DRL_code_pytorch.Rainbow_DQN.Rainbow_DQN_radar_main --blur_radius=2 --cdl=0.9 --epsilon_init=1 &
 python -m rl_agents.DRL_code_pytorch.Rainbow_DQN.Rainbow_DQN_radar_main --blur_radius=1 --use_double=0 --use_dueling=0 --use_noisy=0 --use_per=0 --use_n_steps=0 --cdl=0.9 --epsilon_init=1 &
 python -m rl_agents.DRL_code_pytorch.Rainbow_DQN.Rainbow_DQN_radar_main --blur_radius=2 --use_double=0 --use_dueling=0 --use_noisy=0 --use_per=0 --use_n_steps=0 --cdl=0.9 --epsilon_init=1 &
+python -m rl_agents.DRL_code_pytorch.Rainbow_DQN.Rainbow_DQN_radar_main --blur_radius=1 --use_double=0 --use_dueling=0 --use_noisy=0 --use_per=0 --use_n_steps=1 --cdl=0.9 --epsilon_init=1 &
+python -m rl_agents.DRL_code_pytorch.Rainbow_DQN.Rainbow_DQN_radar_main --blur_radius=1 --use_double=1 --use_dueling=0 --use_noisy=0 --use_per=0 --use_n_steps=0 --cdl=0.9 --epsilon_init=1
 wait
 echo next 4
 python -m rl_agents.DRL_code_pytorch.Rainbow_DQN.Rainbow_DQN_radar_main --blur_radius=2 --use_double=1 --use_dueling=0 --use_noisy=0 --use_per=0 --use_n_steps=0 --cdl=0.9 --epsilon_init=1 &
@@ -28,11 +24,5 @@ python -m rl_agents.DRL_code_pytorch.Rainbow_DQN.Rainbow_DQN_radar_main --blur_r
 wait
 echo next 4
 python -m rl_agents.DRL_code_pytorch.Rainbow_DQN.Rainbow_DQN_radar_main --blur_radius=1 --use_double=0 --use_dueling=0 --use_noisy=1 --use_per=0 --use_n_steps=0 --cdl=0.9 --epsilon_init=1 &
-python -m rl_agents.DRL_code_pytorch.Rainbow_DQN.Rainbow_DQN_radar_main --blur_radius=2 --use_double=0 --use_dueling=0 --use_noisy=0 --use_per=1 --use_n_steps=0 --cdl=0.9 --epsilon_init=1 &
-python -m rl_agents.DRL_code_pytorch.Rainbow_DQN.Rainbow_DQN_radar_main --blur_radius=1 --use_double=0 --use_dueling=0 --use_noisy=0 --use_per=1 --use_n_steps=0 --cdl=0.9 --epsilon_init=1 &
 python -m rl_agents.DRL_code_pytorch.Rainbow_DQN.Rainbow_DQN_radar_main --blur_radius=2 --use_double=0 --use_dueling=0 --use_noisy=0 --use_per=0 --use_n_steps=1 --cdl=0.9 --epsilon_init=1
-wait
-echo next 4
-python -m rl_agents.DRL_code_pytorch.Rainbow_DQN.Rainbow_DQN_radar_main --blur_radius=1 --use_double=0 --use_dueling=0 --use_noisy=0 --use_per=0 --use_n_steps=1 --cdl=0.9 --epsilon_init=1 &
-python -m rl_agents.DRL_code_pytorch.Rainbow_DQN.Rainbow_DQN_radar_main --blur_radius=1 --use_double=1 --use_dueling=0 --use_noisy=0 --use_per=0 --use_n_steps=0 --cdl=0.9 --epsilon_init=1
 wait
