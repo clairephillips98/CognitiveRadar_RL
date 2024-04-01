588525ec5cf3b70f2eba84d0d09f1eca3aa7b2cf
clairephillips98 <clairephillips98@gmail.com>
2024-04-01 15:44:53 -0400

    higher epsilon trial

--- gpu_serial_job.sh
+++ gpu_serial_job.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-name='_airport_cond__a8_t30_r2'
+name='_airport_cond__a8_t30_r2_e1'
 radars=2
 sbatch <<EOT &
 #!/bin/bash
@@ -11,7 +11,7 @@ sbatch <<EOT &
 module load anaconda3
 echo 1
 source activate pytorch_env
-python -m rl_agents.DRL_code_pytorch.Rainbow_DQN.Rainbow_DQN_radar_main --cdl=1 --epsilon_init=1 --epsilon_init=0.5 --load_model=0 --env_name="$name" --speed_layer=0 --speed_scale=1 --agents=1 --radars=2
+python -m rl_agents.DRL_code_pytorch.Rainbow_DQN.Rainbow_DQN_radar_main --cdl=1 --epsilon_init=1 --load_model=0 --env_name="$name" --speed_layer=0 --speed_scale=1 --agents=1 --radars=2
 EOT
 sbatch <<EOT &
 #!/bin/bash
@@ -23,16 +23,5 @@ sbatch <<EOT &
 module load anaconda3
 echo 1
 source activate pytorch_env
-python -m rl_agents.DRL_code_pytorch.Rainbow_DQN.Rainbow_DQN_radar_main --cdl=1 --epsilon_init=1 --epsilon_init=0.5 --load_model=0 --env_name="$name" --speed_layer=0 --speed_scale=1 --agents=2 --radars=2
-EOTsbatch <<EOT &
-#!/bin/bash
-#SBATCH --nodes=1
-#SBATCH --gpus-per-node=1
-#SBATCH --time=9:00:0
-#SBATCH --export=ALL
-#SBATCH --output=cphil_test_apr1_3.txt
-module load anaconda3
-echo 1
-source activate pytorch_env
-python -m rl_agents.baseline_model --cdl=1 --env_name="$name" --speed_scale=1 --radars=2
+python -m rl_agents.DRL_code_pytorch.Rainbow_DQN.Rainbow_DQN_radar_main --cdl=1 --epsilon_init=1 --load_model=0 --env_name="$name" --speed_layer=0 --speed_scale=1 --agents=2 --radars=2
 EOT
