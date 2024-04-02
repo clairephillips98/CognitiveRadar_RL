de6fda9d989ba19a67e6674e9e371ca84c079249
clairephillips98 <clairephillips98@gmail.com>
2024-04-02 12:44:42 -0400

    fixing bug

--- gpu_serial_job.sh
+++ gpu_serial_job.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-name='exp3_airport_cond_a8_t30_r2_e1'
+name='exp2_airport_cond_a8_t30_r2_e1'
 radars=2
 sbatch <<EOT &
 #!/bin/bash
@@ -7,12 +7,15 @@ sbatch <<EOT &
 #SBATCH --gpus-per-node=1
 #SBATCH --time=9:00:0
 #SBATCH --export=ALL
-#SBATCH --output=cphil_test_apr2_5.txt
+#SBATCH --output=cphil_test_apr2_2.txt
 module load anaconda3
 echo 1
 source activate pytorch_env
-python -m rl_agents.DRL_code_pytorch.Rainbow_DQN.Rainbow_DQN_radar_main --cdl=1 --epsilon_init=1 --load_model=0 --env_name="$name" --speed_layer=1 --speed_scale=1 --agents=1 --radars=2
+python -m rl_agents.DRL_code_pytorch.Rainbow_DQN.Rainbow_DQN_radar_main --cdl=1 --epsilon_init=1 --load_model=0 --env_name="$name" --speed_layer=1 --speed_scale=1000 --agents=2 --radars=2
 EOT
+
+name='exp2_airport_cond_a8_t30_r2_e1'
+
 sbatch <<EOT &
 #!/bin/bash
 #SBATCH --nodes=1
@@ -24,28 +27,4 @@ module load anaconda3
 echo 1
 source activate pytorch_env
 python -m rl_agents.DRL_code_pytorch.Rainbow_DQN.Rainbow_DQN_radar_main --cdl=1 --epsilon_init=1 --load_model=0 --env_name="$name" --speed_layer=1 --speed_scale=1 --agents=2 --radars=2
-EOT
-sbatch <<EOT &
-#!/bin/bash
-#SBATCH --nodes=1
-#SBATCH --gpus-per-node=1
-#SBATCH --time=9:00:0
-#SBATCH --export=ALL
-#SBATCH --output=cphil_test_apr2_7.txt
-module load anaconda3
-echo 1
-source activate pytorch_env
-python -m rl_agents.baseline_model --cdl=1 --env_name="$name" --speed_scale=1 --radars=2
-EOT
-sbatch <<EOT &
-#!/bin/bash
-#SBATCH --nodes=1
-#SBATCH --gpus-per-node=1
-#SBATCH --time=9:00:0
-#SBATCH --export=ALL
-#SBATCH --output=cphil_test_apr2_8.txt
-module load anaconda3
-echo 1
-source activate pytorch_env
-python -m rl_agents.baseline_model --cdl=1 --env_name="$name" --speed_scale=1 --radars=2 --baseline_model_type="no_movement"
 EOT
