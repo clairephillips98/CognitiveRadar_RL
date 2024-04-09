423b163829dc61b05ad4c6853458d7ae5974bf46
clairephillips98 <clairephillips98@gmail.com>
2024-04-09 15:10:12 -0400

    fix 1 radar bug

--- gpu_serial_job.sh
+++ gpu_serial_job.sh
@@ -1,18 +1,5 @@
 #!/bin/bash
 name='penalty_airport_cond_a8_t30_r2_e0.5'
-
-sbatch <<EOT &
-#!/bin/bash
-#SBATCH --nodes=1
-#SBATCH --gpus-per-node=1
-#SBATCH --time=9:00:0
-#SBATCH --export=ALL
-#SBATCH --output=cphil_test_apr9_1.txt
-module load anaconda3
-echo 1
-source activate pytorch_env
-python -m rl_agents.baseline_model --env_name="$name" --speed_scale=1 --radars=2 --baseline_model_type="no_movement" --penalize_no_movement=1
-EOT
 sbatch <<EOT &
 #!/bin/bash
 #SBATCH --nodes=1
@@ -60,52 +47,4 @@ module load anaconda3
 echo 1
 source activate pytorch_env
 python -m rl_agents.baseline_model --env_name="$name" --speed_scale=1 --radars=1 --baseline_model_type="simple" --penalize_no_movement=1
-EOT
-sbatch <<EOT &
-#!/bin/bash
-#SBATCH --nodes=1
-#SBATCH --gpus-per-node=1
-#SBATCH --time=9:00:0
-#SBATCH --export=ALL
-#SBATCH --output=cphil_test_apr9_6.txt
-module load anaconda3
-echo 1
-source activate pytorch_env
-python -m rl_agents.DRL_code_pytorch.Rainbow_DQN.Rainbow_DQN_radar_main --blur_radius=1 --cdl=1 --epsilon_init=0.5 --load_model=0 --speed_scale=1 --env_name="$name" --penalize_no_movement=1 --radars=2 --agents=1 --relative_change=1
-EOT
-sbatch <<EOT &
-#!/bin/bash
-#SBATCH --nodes=1
-#SBATCH --gpus-per-node=1
-#SBATCH --time=9:00:0
-#SBATCH --export=ALL
-#SBATCH --output=cphil_test_apr9_7.txt
-module load anaconda3
-echo 1
-source activate pytorch_env
-python -m rl_agents.DRL_code_pytorch.Rainbow_DQN.Rainbow_DQN_radar_main --blur_radius=1 --cdl=1 --epsilon_init=0.5 --load_model=0 --speed_scale=1 --env_name="$name" --penalize_no_movement=1 --radars=2 --agents=2 --relative_change=1
-EOT
-sbatch <<EOT &
-#!/bin/bash
-#SBATCH --nodes=1
-#SBATCH --gpus-per-node=1
-#SBATCH --time=9:00:0
-#SBATCH --export=ALL
-#SBATCH --output=cphil_test_apr9_8.txt
-module load anaconda3
-echo 1
-source activate pytorch_env
-python -m rl_agents.DRL_code_pytorch.Rainbow_DQN.Rainbow_DQN_radar_main --blur_radius=1 --cdl=1 --epsilon_init=0.5 --load_model=0 --speed_scale=1 --env_name="$name" --penalize_no_movement=1 --radars=2 --agents=1 --relative_change=0
-EOT
-sbatch <<EOT &
-#!/bin/bash
-#SBATCH --nodes=1
-#SBATCH --gpus-per-node=1
-#SBATCH --time=9:00:0
-#SBATCH --export=ALL
-#SBATCH --output=cphil_test_apr9_9.txt
-module load anaconda3
-echo 1
-source activate pytorch_env
-python -m rl_agents.DRL_code_pytorch.Rainbow_DQN.Rainbow_DQN_radar_main --blur_radius=1 --cdl=1 --epsilon_init=0.5 --load_model=0 --speed_scale=1 --env_name="$name" --penalize_no_movement=1 --radars=2 --agents=2 --relative_change=0
 EOT
