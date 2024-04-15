26b2dc6d561f76b494d3a06895ab85dfe144b27e
clairephillips98 <clairephillips98@gmail.com>
2024-04-15 16:29:09 -0400

    fixing baseline bug

--- gpu_serial_job.sh
+++ gpu_serial_job.sh
@@ -27,3 +27,27 @@ source activate pytorch_env
 # 2 radars, nm, pnm
 python -m rl_agents.baseline_model --env_name="$name" --speed_scale=1 --radars=2 --baseline_model_type="no_movement" --penalize_no_movement=1
 EOT
+sbatch <<EOT &
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --gpus-per-node=1
+#SBATCH --time=9:00:0
+#SBATCH --export=ALL
+#SBATCH --output=cphil_test_apr15_4.txt
+module load anaconda3
+echo 1
+source activate pytorch_env
+python -m rl_agents.baseline_model --env_name="$name" --speed_scale=1 --radars=1 --baseline_model_type="no_movement" --penalize_no_movement=1
+EOT
+sbatch <<EOT &
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --gpus-per-node=1
+#SBATCH --time=9:00:0
+#SBATCH --export=ALL
+#SBATCH --output=cphil_test_apr15_5.txt
+module load anaconda3
+echo 1
+source activate pytorch_env
+python -m rl_agents.baseline_model --env_name="$name" --speed_scale=1 --radars=1 --baseline_model_type="simple" --penalize_no_movement=1
+EOT
