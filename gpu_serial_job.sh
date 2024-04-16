0bf079cd680ddc2a816c284a0bfbcc3ffaa0a418
clairephillips98 <clairephillips98@gmail.com>
2024-04-16 13:34:12 -0400

    running the diff MARL jobs

--- gpu_serial_job.sh
+++ gpu_serial_job.sh
@@ -1,6 +1,128 @@
 #!/bin/bash
-name='a16_penalty_airport_cond_a8_t30_e0.5'
+name='180_apart_a16_penalty_airport_cond_a8_t30'
 radars=2
+
+sbatch <<EOT &
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --gpus-per-node=1
+#SBATCH --time=9:00:0
+#SBATCH --export=ALL
+#SBATCH --output=cphil_test_apr16_0.txt
+module load anaconda3
+echo 1
+source activate pytorch_env
+python -m rl_agents.baseline_model --env_name="$name" --speed_scale=1 --radars=2 --baseline_model_type="simple" --penalize_no_movement=1
+EOT
+sbatch <<EOT &
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --gpus-per-node=1
+#SBATCH --time=9:00:0
+#SBATCH --export=ALL
+#SBATCH --output=cphil_test_apr16_1.txt
+module load anaconda3
+echo 1
+source activate pytorch_env
+# 2 radars, nm, pnm
+python -m rl_agents.baseline_model --env_name="$name" --speed_scale=1 --radars=2 --baseline_model_type="no_movement" --penalize_no_movement=1
+EOT
+sbatch <<EOT &
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --gpus-per-node=1
+#SBATCH --time=9:00:0
+#SBATCH --export=ALL
+#SBATCH --output=cphil_test_apr16_2.txt
+module load anaconda3
+echo 1
+source activate pytorch_env
+python -m rl_agents.DRL_code_pytorch.Rainbow_DQN.Rainbow_DQN_radar_main --blur_radius=1 --cdl=1 --epsilon_init=0.5 --load_model=0 --speed_scale=1 --env_name="$name" --penalize_no_movement=1 --radars=1 --agents=1 --relative_change=1
+EOT
+sbatch <<EOT &
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --gpus-per-node=1
+#SBATCH --time=9:00:0
+#SBATCH --export=ALL
+#SBATCH --output=cphil_test_apr16_3.txt
+module load anaconda3
+echo 1
+source activate pytorch_env
+python -m rl_agents.DRL_code_pytorch.Rainbow_DQN.Rainbow_DQN_radar_main --blur_radius=1 --cdl=1 --epsilon_init=0.5 --load_model=0 --speed_scale=1 --env_name="$name" --penalize_no_movement=1 --radars=1 --agents=1 --relative_change=0
+EOT
+sbatch <<EOT &
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --gpus-per-node=1
+#SBATCH --time=9:00:0
+#SBATCH --export=ALL
+#SBATCH --output=cphil_test_apr16_4.txt
+module load anaconda3
+echo 1
+source activate pytorch_env
+python -m rl_agents.baseline_model --env_name="$name" --speed_scale=1 --radars=1 --baseline_model_type="no_movement" --penalize_no_movement=1
+EOT
+sbatch <<EOT &
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --gpus-per-node=1
+#SBATCH --time=9:00:0
+#SBATCH --export=ALL
+#SBATCH --output=cphil_test_apr16_5.txt
+module load anaconda3
+echo 1
+source activate pytorch_env
+python -m rl_agents.baseline_model --env_name="$name" --speed_scale=1 --radars=1 --baseline_model_type="simple" --penalize_no_movement=1
+EOT
+sbatch <<EOT &
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --gpus-per-node=1
+#SBATCH --time=9:00:0
+#SBATCH --export=ALL
+#SBATCH --output=cphil_test_apr16_6.txt
+module load anaconda3
+echo 1
+source activate pytorch_env
+python -m rl_agents.DRL_code_pytorch.Rainbow_DQN.Rainbow_DQN_radar_main --blur_radius=1 --cdl=1 --epsilon_init=0.5 --load_model=0 --speed_scale=1 --env_name="$name" --penalize_no_movement=1 --radars=2 --agents=1 --relative_change=1
+EOT
+sbatch <<EOT &
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --gpus-per-node=1
+#SBATCH --time=9:00:0
+#SBATCH --export=ALL
+#SBATCH --output=cphil_test_apr16_7.txt
+module load anaconda3
+echo 1
+source activate pytorch_env
+python -m rl_agents.DRL_code_pytorch.Rainbow_DQN.Rainbow_DQN_radar_main --blur_radius=1 --cdl=1 --epsilon_init=0.5 --load_model=0 --speed_scale=1 --env_name="$name" --penalize_no_movement=1 --radars=2 --agents=2 --relative_change=1
+EOT
+sbatch <<EOT &
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --gpus-per-node=1
+#SBATCH --time=9:00:0
+#SBATCH --export=ALL
+#SBATCH --output=cphil_test_apr16_8.txt
+module load anaconda3
+echo 1
+source activate pytorch_env
+python -m rl_agents.DRL_code_pytorch.Rainbow_DQN.Rainbow_DQN_radar_main --blur_radius=1 --cdl=1 --epsilon_init=0.5 --load_model=0 --speed_scale=1 --env_name="$name" --penalize_no_movement=1 --radars=2 --agents=1 --relative_change=0
+EOT
+sbatch <<EOT &
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --gpus-per-node=1
+#SBATCH --time=9:00:0
+#SBATCH --export=ALL
+#SBATCH --output=cphil_test_apr16_9.txt
+module load anaconda3
+echo 1
+source activate pytorch_env
+python -m rl_agents.DRL_code_pytorch.Rainbow_DQN.Rainbow_DQN_radar_main --blur_radius=1 --cdl=1 --epsilon_init=0.5 --load_model=0 --speed_scale=1 --env_name="$name" --penalize_no_movement=1 --radars=2 --agents=2 --relative_change=0
+EOT
 sbatch <<EOT &
 #!/bin/bash
 #SBATCH --nodes=1
