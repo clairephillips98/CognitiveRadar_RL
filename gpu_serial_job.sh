4af30b712ea59c2865c1b1157dcb70cbe732668c
clairephillips98 <clairephillips98@gmail.com>
2024-04-23 12:00:18 -0400

    single radar trials

--- gpu_serial_job.sh
+++ gpu_serial_job.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 # Iterate over the sequence of floating-point numbers
-for r in 1 2; do #radars
-    for bl in 1 2 3 4 5; do #bl
-        for i in 1 3 5; do #ss
+for r in 0 1; do #relative
+    for i in 0 1 5; do #ss
+        for m in 64 128 256; do #hidden dim
+          for n in 1 3 5; do #nstep
             name='a19_penalty_airport_cond_a8_t30_unmask_0.1_'
-            full_name="${name}r${r}_bl${bl}_ss${i}"
-            echo "$full_name"
+            echo "name"
 
             sbatch <<EOT
 #!/bin/bash
 #SBATCH --nodes=1
 #SBATCH --gpus-per-node=1
-#SBATCH --time=1:00:0
+#SBATCH --time=6:00:0
 #SBATCH --export=ALL
 #SBATCH --output=cphil_test_apr16_${i}.txt
 
@@ -24,15 +24,17 @@ python -m rl_agents.DRL_code_pytorch.Rainbow_DQN.Rainbow_DQN_radar_main \
     --epsilon_init=0.5 \
     --load_model=0 \
     --speed_scale=1 \
-    --env_name="$full_name" \
+    --env_name="name" \
     --penalize_no_movement=1 \
-    --radars=$r \
+    --radars=1 \
     --agents=1 \
-    --baseline=$bl \
+    --baseline=0 \
     --outside_radar_value=0.9 \
     --blur_sigma=0.5 \
-    --relative_change=0 \
+    --relative_change=$r \
     --speed_scale=$i
+    --n_steps=$n \
+    --hidden_dim=$m \
 EOT
         done
     done
