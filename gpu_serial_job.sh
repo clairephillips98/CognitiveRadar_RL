9c3a1e1f68c7919fb347c4cf40baf7ecf6ce5ca2
clairephillips98 <clairephillips98@gmail.com>
2024-04-25 15:41:32 -0400

    changing outside to be 0.7

--- gpu_serial_job.sh
+++ gpu_serial_job.sh
@@ -2,10 +2,10 @@
 
 
 # Iterate over the sequence of floating-point numbers
-os = 0.8
+
 for p in 0 1; do
   for bl in 1 2 3 4 5; do #bl
-          full_name="a25_summed_reward_os_0.8"
+          full_name="a25_summed_reward_os_0.7"
           echo "$full_name"
           sbatch <<EOT
 #!/bin/bash
@@ -22,12 +22,12 @@ python -m rl_agents.DRL_code_pytorch.Rainbow_DQN.Rainbow_DQN_radar_main \
     --cdl=1 \
     --epsilon_init=0.5 \
     --load_model=0 \
-    --env_name="$full_name" \
+    --env_name="a25_summed_reward_os_0.7"\
     --penalize_no_movement=$p \
     --radars=1 \
     --agents=1 \
     --baseline=$bl \
-    --outside_radar_value=$os \
+    --outside_radar_value=0.7 \
     --blur_sigma=0.5 \
     --relative_change=0 \
     --speed_scale=1 \
@@ -37,19 +37,19 @@ python -m rl_agents.DRL_code_pytorch.Rainbow_DQN.Rainbow_DQN_radar_main \
     --cdl=1 \
     --epsilon_init=0.5 \
     --load_model=0 \
-    --env_name="$full_name" \
+    --env_name="a25_summed_reward_os_0.7" \
     --penalize_no_movement=$p \
     --radars=2 \
     --agents=1 \
     --baseline=$bl \
-    --outside_radar_value=0.8 \
+    --outside_radar_value=0.7 \
     --blur_sigma=0.5 \
     --relative_change=0 \
     --speed_scale=1 \
     --max_train_steps=200000
 EOT
   done
-  for m in 128; do
+  for m in 64; do
       for n in 1 7; do
         for rc in 0 1; do
           name='a25_summed_reward'
@@ -61,7 +61,7 @@ EOT
 #SBATCH --gpus-per-node=1
 #SBATCH --time=8:00:0
 #SBATCH --export=ALL
-#SBATCH --output=cphil_test_apr16_${os}${m}${n}.txt
+#SBATCH --output=cphil_test_apr16_0.7${m}${n}.txt
 
 module load anaconda3
 source activate pytorch_env
@@ -70,12 +70,12 @@ python -m rl_agents.DRL_code_pytorch.Rainbow_DQN.Rainbow_DQN_radar_main \
     --cdl=1 \
     --epsilon_init=0.5 \
     --load_model=0 \
-    --env_name="$full_name" \
+    --env_name="a25_summed_reward_os_0.7" \
     --penalize_no_movement=$p \
     --radars=1 \
     --agents=1 \
     --baseline=0 \
-    --outside_radar_value=0.8 \
+    --outside_radar_value=0.7 \
     --blur_sigma=0.5 \
     --relative_change=$rc \
     --speed_scale=1 \
@@ -87,7 +87,7 @@ EOT
       done
     done
   done
-  os = 0.8
+  os = 0.7
   for n in 1 7; do
     for m in 128; do
       for rc in 0 1; do
@@ -108,12 +108,12 @@ python -m rl_agents.DRL_code_pytorch.Rainbow_DQN.Rainbow_DQN_radar_main \
     --cdl=1 \
     --epsilon_init=0.5 \
     --load_model=0 \
-    --env_name="$full_name" \
+    --env_name="a25_summed_reward_os_0.7" \
     --penalize_no_movement=$p \
     --radars=2 \
     --agents=1 \
     --baseline=0 \
-    --outside_radar_value=0.8 \
+    --outside_radar_value=0.7 \
     --blur_sigma=0.5 \
     --relative_change=$rc \
     --speed_scale=1 \
@@ -138,12 +138,12 @@ python -m rl_agents.DRL_code_pytorch.Rainbow_DQN.Rainbow_DQN_radar_main \
     --cdl=1 \
     --epsilon_init=0.5 \
     --load_model=0 \
-    --env_name="$full_name" \
+    --env_name="a25_summed_reward_os_0.7" \
     --penalize_no_movement=$p \
     --radars=2 \
     --agents=2 \
     --baseline=0 \
-    --outside_radar_value=0.8 \
+    --outside_radar_value=0.7 \
     --blur_sigma=0.5 \
     --relative_change=$rc \
     --speed_scale=1 \
