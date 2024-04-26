1809a97940a4b6e0b65ca99e2e25fcb70a0c6e35
clairephillips98 <clairephillips98@gmail.com>
2024-04-26 10:04:29 -0400

    2 radar jobs

--- gpu_serial_job.sh
+++ gpu_serial_job.sh
@@ -3,89 +3,8 @@
 
 # Iterate over the sequence of floating-point numbers
 
-for bl in 1 2 3 4 5; do #bl
-        full_name="a25_reward_os_0.7"
-        echo "$full_name"
-        sbatch <<EOT
-#!/bin/bash
-#SBATCH --nodes=1
-#SBATCH --gpus-per-node=1
-#SBATCH --time=2:00:0
-#SBATCH --export=ALL
-#SBATCH --output=cphil_test_apr16_${os}${p}${b}.txt
 
-module load anaconda3
-source activate pytorch_env
-python -m rl_agents.DRL_code_pytorch.Rainbow_DQN.Rainbow_DQN_radar_main \
-    --blur_radius=1 \
-    --cdl=1 \
-    --epsilon_init=0.5 \
-    --load_model=0 \
-    --env_name="a25_summed_reward_os_0.7"\
-    --penalize_no_movement=$p \
-    --radars=1 \
-    --agents=1 \
-    --baseline=$bl \
-    --outside_radar_value=0.7 \
-    --blur_sigma=0.5 \
-    --relative_change=0 \
-    --speed_scale=1 \
-    --max_train_steps=200000
-python -m rl_agents.DRL_code_pytorch.Rainbow_DQN.Rainbow_DQN_radar_main \
-    --blur_radius=1 \
-    --cdl=1 \
-    --epsilon_init=0.5 \
-    --load_model=0 \
-    --env_name="a25_summed_reward_os_0.7" \
-    --penalize_no_movement=$p \
-    --radars=2 \
-    --agents=1 \
-    --baseline=$bl \
-    --outside_radar_value=0.7 \
-    --blur_sigma=0.5 \
-    --relative_change=0 \
-    --speed_scale=1 \
-    --max_train_steps=200000
-EOT
-done
-for m in 64 128; do
-    for n in 1 7; do
-      for rc in 0 1; do
-        name='a25_reward_os_0.7'
-        full_name="${name}_nstep_${n}_hd_${m}"
-        echo "$full_name"
-        sbatch <<EOT
-#!/bin/bash
-#SBATCH --nodes=1
-#SBATCH --gpus-per-node=1
-#SBATCH --time=8:00:0
-#SBATCH --export=ALL
-#SBATCH --output=cphil_test_apr16_0.7${m}${n}.txt
-
-module load anaconda3
-source activate pytorch_env
-python -m rl_agents.DRL_code_pytorch.Rainbow_DQN.Rainbow_DQN_radar_main \
-    --blur_radius=1 \
-    --cdl=1 \
-    --epsilon_init=0.5 \
-    --load_model=0 \
-    --env_name="a25_summed_reward_os_0.7" \
-    --penalize_no_movement=$p \
-    --radars=1 \
-    --agents=1 \
-    --baseline=0 \
-    --outside_radar_value=0.7 \
-    --blur_sigma=0.5 \
-    --relative_change=$rc \
-    --speed_scale=1 \
-    --hidden_dim=$m \
-    --n_steps=$n
-    --max_train_steps=2000000
-EOT
-      done
-    done
-  done
-os = 0.7
+os=0.7
 for n in 1 7; do
   for m in 64 128; do
     for rc in 0 1; do
@@ -153,3 +72,4 @@ EOT
       done
     done
   done
+done
