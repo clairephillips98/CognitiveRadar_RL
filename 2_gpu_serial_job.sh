edf9ab711cdd250e143a271678d948f17c06a1ac
clairephillips98 <clairephillips98@gmail.com>
2024-05-08 16:27:24 -0400

    rerun stuff

--- 2_gpu_serial_job.sh
+++ 2_gpu_serial_job.sh
@@ -48,41 +48,12 @@ done
 
 for noisy in 0 1; do #bs
   for hl in 128 264 ; do #hidden layer
-    full_name="5_targets_a15_ns1_hd${hl}"
+    full_name=full_name="double_SCALING_T5_a15_ns1_hd${hl}"
     sbatch <<EOT &
 #!/bin/bash
 #SBATCH --nodes=1
 #SBATCH --gpus-per-node=1
-#SBATCH --time=16:00:0
-#SBATCH --export=ALL
-#SBATCH --output=run.txt
-module load anaconda3
-source activate pytorch_env
-python -m rl_agents.DRL_code_pytorch.Rainbow_DQN.Rainbow_DQN_radar_main \
-    --blur_radius=1 \
-    --cdl=0 \
-    --epsilon_init=0.5 \
-    --load_model=0 \
-    --env_name="${full_name}" \
-    --penalize_no_movement=1 \
-    --radars=1 \
-    --agents=1 \
-    --baseline=0 \
-    --outside_radar_value=0.2 \
-    --blur_sigma=0.5 \
-    --relative_change=0\
-    --speed_scale=2 \
-    --use_noisy=$noisy \
-    --hidden_dim=$hl \
-    --n_steps=$nstep \
-    --max_train_steps=3000000
-EOT
-    full_name="5_targets_a15_ns1_hd${hl}"
-    sbatch <<EOT &
-#!/bin/bash
-#SBATCH --nodes=1
-#SBATCH --gpus-per-node=1
-#SBATCH --time=25:00:0
+#SBATCH --time=24:00:0
 #SBATCH --export=ALL
 #SBATCH --output=run.txt
 module load anaconda3
@@ -102,6 +73,35 @@ python -m rl_agents.DRL_code_pytorch.Rainbow_DQN.Rainbow_DQN_radar_main \
     --relative_change=0 \
     --use_noisy=0 \
     --speed_scale=2 \
+    --hidden_dim=$hl \
+    --n_steps=1 \
+    --max_train_steps=3000000
+EOT
+    tempName="5_targets_a15_ns1_hd${hl}"
+    SBATCH <<EOT &
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --gpus-per-node=1
+#SBATCH --time=24:00:0
+#SBATCH --export=ALL
+#SBATCH --output=run.txt
+module load anaconda3
+source activate pytorch_env
+python -m rl_agents.DRL_code_pytorch.Rainbow_DQN.Rainbow_DQN_radar_main \
+    --blur_radius=1 \
+    --cdl=0 \
+    --epsilon_init=0.5 \
+    --load_model=0 \
+    --env_name="${tempName}" \
+    --penalize_no_movement=1 \
+    --radars=2 \
+    --agents=1 \
+    --baseline=0 \
+    --outside_radar_value=0.2 \
+    --blur_sigma=$bs \
+    --relative_change=0 \
+    --use_noisy=0 \
+    --speed_scale=1 \
     --hidden_dim=$hl \
     --n_steps=1 \
     --max_train_steps=3000000
@@ -130,9 +130,9 @@ python -m rl_agents.DRL_code_pytorch.Rainbow_DQN.Rainbow_DQN_radar_main \
     --outside_radar_value=0.2 \
     --blur_sigma=0.5 \
     --relative_change=0 \
-    --speed_scale=1 \
+    --speed_scale=2 \
     --hidden_dim=$hl \
-    --n_steps=2 \
+    --n_steps=1 \
     --type_of_MARL=$marl\
     --max_train_steps=3000000
 EOT
