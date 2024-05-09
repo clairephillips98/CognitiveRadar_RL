f075dc6f0cfb589dab3d19750b32161c10c604bb
clairephillips98 <clairephillips98@gmail.com>
2024-05-09 12:15:47 -0400

    use noisy 1 for the ones i messed up

--- gpu_serial_job.sh
+++ gpu_serial_job.sh
@@ -2,6 +2,33 @@ for bs in 0.3 0.5; do
   for ss in 1 2; do
       for hl in 128 264 ; do #hidden layer
         full_name="T5_a15_ns1_hd${hl}"
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --gpus-per-node=1
+#SBATCH --time=18:00:0
+#SBATCH --export=ALL
+#SBATCH --output=run.txt
+module load anaconda3
+source activate pytorch_env
+python -m rl_agents.DRL_code_pytorch.Rainbow_DQN.Rainbow_DQN_radar_main \
+      --blur_radius=1 \
+      --cdl=0 \
+      --epsilon_init=0.5 \
+      --load_model=0 \
+      --env_name="${full_name}" \
+      --penalize_no_movement=1 \
+      --radars=1 \
+      --agents=1 \
+      --baseline=0 \
+      --outside_radar_value=0.2 \
+      --blur_sigma=$bs \
+      --relative_change=0 \
+      --use_noisy=1 \
+      --speed_scale=$ss \
+      --hidden_dim=$hl \
+      --n_steps=1 \
+      --max_train_steps=3000000
+EOT
     sbatch <<EOT &
 #!/bin/bash
 #SBATCH --nodes=1
