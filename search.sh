772799f116d54a4007cb68c1bbcab1c990cda55f
clairephillips98 <clairephillips98@gmail.com>
2024-06-28 14:26:07 -0400

    run sh again?

--- search.sh
+++ search.sh
@@ -1,11 +1,11 @@
-for bl in 1 2 3 4 5 ; do #hidden layer
-  for rad in 1 2; do
+for hl in 128 264 ; do #hidden layer
+  for marl in "some_shared_info" "some_shared_info_shared_reward" "shared_targets_only" "single_agent"; do
     full_name="SEARCH_REDOING_FINAL_hd${hl}"
     sbatch <<EOT &
 #!/bin/bash
 #SBATCH --nodes=1
 #SBATCH --gpus-per-node=1
-#SBATCH --time=4:00:0
+#SBATCH --time=24:00:0
 #SBATCH --export=ALL
 #SBATCH --output=run.txt
 module load anaconda3
@@ -17,17 +17,20 @@ python -m rl_agents.DRL_code_pytorch.Rainbow_DQN.Rainbow_DQN_radar_main \
       --load_model=0 \
       --env_name="${full_name}" \
       --penalize_no_movement=1 \
-      --radars=$rad \
-      --agents=1 \
-      --baseline=$bl \
-      --outside_radar_value=0.0 \
+      --radars=2 \
+      --agents=2 \
+      --baseline=0 \
+      --outside_radar_value=0.0\
       --blur_sigma=0.3 \
       --relative_change=0 \
       --use_noisy=1 \
       --speed_scale=2 \
+      --hidden_dim=$hl \
       --n_steps=1 \
-      --max_train_steps=3000000 \
-      --evaluate_freq=500 \
+      --max_train_steps=3000000
+      --type_of_MARL=$marl \
+      --evaluate_freq=500\
+      --evaluate_times=2\
       --search_outer_circle=1
 EOT
   done
