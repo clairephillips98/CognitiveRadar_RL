89d879162f50cbb37704a1abc1c6065fcfaf41c8
clairephillips98 <clairephillips98@gmail.com>
2024-07-04 17:42:36 -0400

    fix

--- gpu_serial_job.sh
+++ gpu_serial_job.sh
@@ -1,5 +1,5 @@
 for bl in 1 2 3 4 5 ; do #hidden layer
-  for rad in 1; do
+  for os in 0.2 0.3 0.5; do
     full_name="GENERAL_REDOING_FINAL_hd${hl}"
     sbatch <<EOT &
 #!/bin/bash
@@ -17,10 +17,10 @@ python -m rl_agents.DRL_code_pytorch.Rainbow_DQN.Rainbow_DQN_radar_main \
       --load_model=0 \
       --env_name="${full_name}" \
       --penalize_no_movement=1 \
-      --radars=$rad \
+      --radars=1 \
       --agents=1 \
       --baseline=$bl \
-      --outside_radar_value=0.2 \
+      --outside_radar_value=$os \
       --blur_sigma=0.3 \
       --relative_change=0 \
       --use_noisy=1 \
